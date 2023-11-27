1bdcce2842b50a481b1f34a5da4e3283baea330c
Himangi Gautam <137421732+coderhg05@users.noreply.github.com>
2023-11-27 22:18:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.querySelector('.mobile-menu')
+const mobileClose = document.querySelector('.mobile-close')
+const mobileNavigation = document.querySelector('.mobile-navigation')
+ 
+mobileMenu.addEventListener('click', () =>{
+    mobileNavigation.classList.add('active');
+    mobileClose.classList.add('active');
+})
+
+mobileClose.addEventListener('click', () =>{
+    mobileNavigation.classList.remove('active');
+    mobileClose.classList.remove('active');
+})
